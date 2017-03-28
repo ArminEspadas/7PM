4f84bfbc6ad11c40daad625486c0f2332a12961f
ArminEspadas <armin_hellsing@hotmail.com>
2017-03-28 01:16:16 -0600

    store

--- app/src/main/java/com/azore/compustore/MainActivity.java
+++ app/src/main/java/com/azore/compustore/MainActivity.java
@@ -1,14 +1,25 @@
 package com.azore.compustore;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.azore.compustore.fiuady.db.CategoryProduct;
+import com.azore.compustore.fiuady.db.Inventory;
+import com.azore.compustore.fiuady.db.InventoryDbSchema;
+import com.azore.compustore.fiuady.db.InventoryHelper;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
     public ImageButton btnclientes;
     public ImageButton btnordenes;
     public ImageButton btnreportes;
+
+    private Inventory inventory;
+    private SQLiteDatabase db;
+    private  InventoryHelper inventoryHelper;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
         btnordenes= (ImageButton)findViewById(R.id.btnOrdenes);
         btnreportes= (ImageButton)findViewById(R.id.btnReportes);
         // DECLARANDO BOTONES
+
+        inventory= new Inventory(getApplicationContext());
+      InventoryHelper.backupDatabaseFile(getApplicationContext());
+
+        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+
+
+       final List<CategoryProduct> categoryProductList= inventory.getAllCategoriesProduct();
+
+       CategoryProduct c= new CategoryProduct(categoryProductList.get(1).getId(),categoryProductList.get(1).getDescription() );
+        Toast.makeText(getApplicationContext(), c.getDescription() , Toast.LENGTH_LONG).show();
+
+
+
     }
 
     // Boton categorias
@@ -64,10 +95,6 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this,ReportesActivity.class);
         startActivity(i);
     }
-    //HHHHHH
-    //gugugbbvvv
-//HHHH
-    //subí un c<mbio test
-//NUEYB
+
 
 }
